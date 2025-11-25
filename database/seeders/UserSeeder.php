559ae61821e2ee5ed1426b952a5acae9362bccab
anaschaeru <anasmaulana9@gmail.com>
2025-11-25 13:52:33 +0700

    update userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,10 @@ class UserSeeder extends Seeder
   {
     // 1. Buat Akun ADMIN
     User::create([
-      'name'      => 'Administrator OSIS',
-      'nisn'      => '00000', // Gunakan angka unik untuk admin
+      'name'      => 'Kesiswaan',
+      'nisn'      => '80808080', // Gunakan angka unik untuk admin
       'kelas'     => 'Staff', // Isi sembarang untuk admin
-      'password'  => Hash::make('admin123'), // Password login
+      'password'  => Hash::make('admin80'), // Password login
       'role'      => 'admin',
       'has_voted' => false,
     ]);
